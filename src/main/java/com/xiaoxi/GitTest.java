c8b7d4b37b3ee3a1649f277d870b5d20f90a06b9
xiaoxi <2495133166@qq.com>
2023-01-28 18:24:01 +0800

    idea second commit

--- src/main/java/com/xiaoxi/GitTest.java
+++ src/main/java/com/xiaoxi/GitTest.java
@@ -3,5 +3,6 @@ package com.xiaoxi;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea-git!");
+        System.out.println("hello idea-git2222!");
     }
 }
