a0b4c4127abc16a77f0859b50530c21119819627
xiaoxi <2495133166@qq.com>
2023-01-28 18:26:10 +0800

    idea third commit

--- src/main/java/com/xiaoxi/GitTest.java
+++ src/main/java/com/xiaoxi/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea-git!");
         System.out.println("hello idea-git2222!");
+        System.out.println("hello idea-git3333!");
     }
 }
